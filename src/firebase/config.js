a224b70e78b57c9ad1d49e3299ad50f7d919b291
Leon van Zyl <leon.vanzyl@gmail.com>
2022-04-25 07:43:13 +0200

    initial

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARu1HT7BwL6VFnTB3rZOK4HwVDXJLucyk",
+  authDomain: "dojo-f3674.firebaseapp.com",
+  projectId: "dojo-f3674",
+  storageBucket: "dojo-f3674.appspot.com",
+  messagingSenderId: "251986738884",
+  appId: "1:251986738884:web:c2af175a28c22389ee9391",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
